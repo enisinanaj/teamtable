b4997624ec267b03ea98403b8071f5a33fc12872
Eni Sinanaj <enisinanaj@outlook.com>
2018-01-28 14:05:31 +0100

    Aggiunta funzionalità di cambio password

--- master/js/modules/settings/settings.run.js
+++ master/js/modules/settings/settings.run.js
@@ -45,7 +45,7 @@
           asideScrollbar: false,
           isCollapsedText: false
         },
-        apiUrl: 'http://teamtablear.it:8080/teamtable/api/',
+        apiUrl: 'http://localhost:8080/teamtable/api/',
         useFullLayout: false,
         hiddenFooter: false,
         offsidebarOpen: false,
--- master/js/modules/user/user.controller.js
+++ master/js/modules/user/user.controller.js
@@ -1,17 +1,55 @@
 (function() {
-    angular.module('user', [])
-        .controller('UserController', UserController);
+  'use strict';
 
-    UserController.$inject = ['UserProfile'];
+  angular.module('app.user', [])
+      .controller('UserController', UserController);
 
-    function UserController(UserProfile) {
-        this.vm = this;
+  UserController.$inject = ['UserProfile', '$rootScope', '$http', '$cookies', '$window', 'AuthenticationService', 'AUTH'];
 
-        activate();
+  function UserController(UserProfile, $rootScope, $http, $cookies, $window, AuthenticationService, AUTH) {
+    var vm = this;
 
-        function activate() {
-            vm.user = UserProfile.fetchUserProfile();
-            console.log(vm.user.username);
+    vm.oldPassword = "";
+    vm.newPassword = "";
+    vm.repeatPassword = "";
+    vm.result = "";
+
+    activate();
+
+    function activate() {
+        vm.user = UserProfile.fetchUserProfile();
+
+        vm.saveUser = saveUser;
+
+        function saveUser() {
+          var newPassword = CryptoJS.SHA256(vm.newPassword).toString();
+          var oldPassword = CryptoJS.SHA256(vm.oldPassword).toString();
+          var repeatPassword = CryptoJS.SHA256(vm.repeatPassword).toString();
+
+          var config = {
+              headers: {
+                  'Content-Type': 'application/json;',
+                  'token': AuthenticationService.generateToken(),
+                  'apiKey': AUTH['api_key']
+              },
+              cache: false
+          };
+
+          $http.post($rootScope.app.apiUrl + "users/" + $rootScope.user.id + "/passwords", {
+            newPassword: newPassword,
+            oldPassword: oldPassword,
+            newPasswordRepeat: repeatPassword
+          }, config)
+            .then(function(response) {
+              if ( !response.data.hRef ) {
+                vm.result = 'Le password non sono corrette!'
+              }else{
+                vm.result = 'Password cambiata correttamente!'
+              }
+            }, function() {
+              vm.result = 'Le password non sono corrette!';
+          });
         }
     }
+  }
 })();
